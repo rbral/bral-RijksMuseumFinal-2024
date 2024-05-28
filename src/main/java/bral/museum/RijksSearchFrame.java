8704881bfc15c0f84f15534468c5d45fc7562ada
rbral <rbral@student.touro.edu>
2024-05-28 13:10:43 -0400

    first page loads, but update method not working

--- src/main/java/bral/museum/RijksSearchFrame.java
+++ src/main/java/bral/museum/RijksSearchFrame.java
@@ -1,17 +1,27 @@
 package bral.museum;
 
+import bral.museum.json.ArtObject;
+import bral.museum.json.ArtObjects;
+import com.andrewoid.ApiKey;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URL;
 
 public class RijksSearchFrame extends JFrame
 {
     private JTextField searchField;
     private JButton nextButton;
     private JButton prevButton;
+    private JPanel mainPanel;
+    private JPanel controlPanel;
+    private JPanel resultsPanel;
     private int currentPage = 1;
     private String currentQuery = "";
     private RijksService service;
@@ -19,27 +29,32 @@ public class RijksSearchFrame extends JFrame
     public RijksSearchFrame()
     {
         setTitle("Rijks");
-        setSize(500, 700);
+        setSize(1100, 1000);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
         service = new RijksServiceFactory().getService();
 
-        JPanel main = new JPanel();
-        main.setLayout(new BorderLayout());
-        setContentPane(main);
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        setContentPane(mainPanel);
 
-        JPanel south = new JPanel();
-        main.add(south, BorderLayout.SOUTH);
-        south.setLayout(new GridLayout(2, 5));
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new BorderLayout());
 
         searchField = new JTextField();
         nextButton = new JButton("Next");
         prevButton = new JButton("Previous");
 
-        main.add(searchField, BorderLayout.CENTER);
-        main.add(prevButton, BorderLayout.WEST);
-        main.add(nextButton, BorderLayout.EAST);
+        controlPanel.add(prevButton, BorderLayout.WEST);
+        controlPanel.add(searchField, BorderLayout.CENTER);
+        controlPanel.add(nextButton, BorderLayout.EAST);
+
+        resultsPanel = new JPanel();
+        resultsPanel.setLayout(new GridLayout(2, 5, 10, 10));
+
+        mainPanel.add(controlPanel, BorderLayout.NORTH);
+        mainPanel.add(new JScrollPane(resultsPanel), BorderLayout.CENTER);
 
         searchField.addKeyListener(new KeyAdapter() {
             @Override
@@ -51,6 +66,8 @@ public class RijksSearchFrame extends JFrame
         });
         nextButton.addActionListener(e -> nextPage());
         prevButton.addActionListener(e -> prevPage());
+
+        loadResults();
     }
 
     private void prevPage() {
@@ -67,14 +84,49 @@ public class RijksSearchFrame extends JFrame
 
     private void performSearch() {
         currentQuery = searchField.getText();
-        currentPage = 1;
+        //currentPage = 1;
         loadResults();
 
     }
 
     private void loadResults()
     {
-        
+        ApiKey apiKey = new ApiKey();
+        ArtObjects artObjectsResponse;
+        resultsPanel.removeAll();
+        if(currentQuery.isEmpty())
+        {
+            artObjectsResponse = service.getCollectionByPage(
+                    apiKey.get(),
+                    currentPage
+            ).blockingGet();
+        } else {
+            artObjectsResponse = service.searchCollectionByQuery(
+                    apiKey.get(),
+                    currentQuery,
+                    currentPage
+            ).blockingGet();
+        }
+
+        for (ArtObject artObject : artObjectsResponse.artObjects)
+        {
+            JLabel label = new JLabel();
+            try
+            {
+                URL url = new URL(artObject.webImage.url);
+                Image image = ImageIO.read(url);
+                Image scaledImage = image.getScaledInstance(200, -1, Image.SCALE_DEFAULT);
+                ImageIcon imageIcon = new ImageIcon(scaledImage);
+                label.setIcon(imageIcon);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            resultsPanel.add(label);
+        }
+        repaint();
+
+
     }
 
 
